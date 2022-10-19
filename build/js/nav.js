cfe0f625bbbffc6bf9eb31364145d7a814ecb883
Maria Camila Troncoso <115896872+mcamila17@users.noreply.github.com>
2022-10-18 22:12:34 -0500

    Delete build directory

--- build/js/nav.js
+++
@@ -1,2 +0,0 @@
-function eventListeners(){document.querySelector(".mobile-menu").addEventListener("click",navegacionResponsive)}function navegacionResponsive(){document.querySelector(".navegacion-principal").classList.toggle("mostrar")}document.addEventListener("DOMContentLoaded",(function(){eventListeners()}));
-//# sourceMappingURL=nav.js.map
