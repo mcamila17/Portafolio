b1fc682ca088a14ddec15936e45e1f363406e427
Maria Camila Troncoso <115896872+mcamila17@users.noreply.github.com>
2023-01-06 12:57:35 -0500

    Add files via upload

---
+++ src/js/nav.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    eventListeners();
+});
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion-principal');
+
+    navegacion.classList.toggle('mostrar')
+}
